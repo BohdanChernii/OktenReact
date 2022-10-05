5b6375ffbc27e7b12d633c7de1a965277ff9936c
promius <bodiachernii@gmail.com>
2022-10-05 19:46:48 +0300

    Redux

--- src/layouts/Mainlayout.js
+++ src/layouts/Mainlayout.js
@@ -1,22 +1,24 @@
 import React from 'react';
 
-import {Link} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
 
-import {Outlet} from "react-router";
+import {Outlet, useLocation, useParams} from "react-router";
 
 const Mainlayout = () => {
+  const location = useLocation()
+  console.log(location.pathname);
   return (
     <>
-      <header style={{display:'flex',justifyContent:"space-evenly",margin:'auto',maxWidth:'1440px'}}>
-        <Link to={'users'}>
+      <header style={{display: 'flex', justifyContent: "space-evenly", margin: 'auto', maxWidth: '1440px'}}>
+        <NavLink to={'users'} className={'link'} >
           Users
-        </Link>
-        <Link to={'posts'}>
+        </NavLink>
+        <NavLink to={'posts'} className={'link'}>
           Posts
-        </Link>
-        <Link to={'comments'}>
+        </NavLink>
+        <NavLink to={'comments'} className={'link'}>
           Comments
-        </Link>
+        </NavLink>
       </header>
       <Outlet/>
     </>
