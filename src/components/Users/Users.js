8b4ba98de84fc57f0864a392fbba3207e6682e2a
promius <bodiachernii@gmail.com>
2022-10-05 18:31:24 +0300

    Redux

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -5,23 +5,25 @@ import {ADD_USERS, CHOOSE_USER} from "../../actions/user.actions";
 
 const Users = () => {
 
-  const [users,setUsers] = useState([])
+  // const [users,setUsers] = useState([])
   let state = useSelector(state => state)
+  const {userReducer} = state
+  const {users} = userReducer
   let dispatch = useDispatch()
 
   useEffect(() => {
-    usersServices.getAll().then(res => dispatch({type: ADD_USERS, payload: res.data})).then(res=>setUsers(res.payload))
+    usersServices.getAll().then(res => dispatch({type: ADD_USERS, payload: res.data}))
   }, [])
 
   return (
     <div className={'parent'}>
-        {users.map(user => (
-          <div className={'child'}>
-            <p>Name: {user.name}</p>
-            <p>UserName: {user.username}</p>
-            <button key={user.id} onClick={() => dispatch({type: CHOOSE_USER, payload: user.id})}>User</button>
-          </div>
-        ))}
+      {users.map(user => (
+        <div className={'child'}>
+          <p>Name: {user.name}</p>
+          <p>UserName: {user.username}</p>
+          <button key={user.id} onClick={() => dispatch({type: CHOOSE_USER, payload: user.id})}>User</button>
+        </div>
+      ))}
     </div>
   );
 };
